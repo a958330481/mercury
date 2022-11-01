792b1c591a068436b20a9b54ff6575e1c198c6d5
zhangkai14 <zhangkai14@corp.netease.com>
2022-11-01 14:51:59 +0800

    feat: 完善脚手架

--- app/main/index.js
+++ app/main/index.js
@@ -1,20 +1,31 @@
 const { app,ipcMain } = require("electron");
 const { createWindow, windowManager } = require("../common/windowManager");
 const { LOAD_TYPE } = require("../common/utils/const");
+const isDev = require("electron-is-dev");
+const path = require('path');
 
 // 创建窗口
-function createWindowOne() {
+function handleCreateWindow() {
+    const config = isDev
+        ? {
+              loadType: LOAD_TYPE.Url,
+              loadUrl: "http://localhost:3000",
+          }
+        : {
+              loadType: LOAD_TYPE.File,
+              loadUrl: path.resolve(__dirname, "../renderer/pages/main/index.html"),
+          };
     createWindow({
-        name: "win1",
+        name: "win",
         with: 600,
         height: 480,
-        loadType:LOAD_TYPE.Url,
-        loadUrl: "http://localhost:3000",
+        loadType: config.loadType,
+        loadUrl: config.loadUrl,
         isOpenDevTools: true,
     });
 }
 
 
 app.whenReady().then(() => { 
-    createWindowOne();
+    handleCreateWindow();
 })
--- app/renderer/src/main/src/App.js
+++ app/renderer/src/main/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+const { ipcRenderer } = window.require("electron");
 
 function App() {
   return (
